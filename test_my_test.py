8cfcfce1ee0a14d9a78891f5c55537c410afeb34
Sapfire65 <cot65@yandex.ru>
2022-09-05 00:15:39 +0300

    Тест авторизации по API (позитивный тест)

--- test_my_test.py
+++ test_my_test.py
@@ -5,8 +5,10 @@ class TestMyTest:
     params = [(3), (5), (12), (18)]
 
     @pytest.mark.parametrize('param', params)
-
     def test_go(self, param):
         count = random.randint(0, 20)
 
         assert count >= param, f"{count} МЕНЬШЕ! -  чем {param}"
+
+
+
